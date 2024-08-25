ece64486b54576224e3d673c1df75aff6f78b618
JPSS14 <jp_scarabelli@hotmail.com>
2024-08-24 22:28:38 -0300

    feat: add decimal pipe

--- src/app/components/card-countrie/card-countrie.component.ts
+++ src/app/components/card-countrie/card-countrie.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Countries } from '../../models/countries.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-countrie',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-countrie.component.html',
   styleUrl: './card-countrie.component.css',
 })
