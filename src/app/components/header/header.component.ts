ffec94d0e2ffd3fb753fe6432fbcb45ba56fe4d4
JPSS14 <jp_scarabelli@hotmail.com>
2024-09-02 11:58:02 -0300

    feat: add SelectLanguage in Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SelectLanguageComponent } from '../select-language/select-language.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SelectLanguageComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
