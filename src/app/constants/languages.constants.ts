182f3442d880e61adeb01e60e2ddc824bfa84c08
JPSS14 <jp_scarabelli@hotmail.com>
2024-09-04 16:40:56 -0300

    feat: add 10 new languages in languages constant

--- src/app/constants/languages.constants.ts
+++ src/app/constants/languages.constants.ts
@@ -3,4 +3,22 @@ import { Languages } from '../models/languages.model';
 export const LANGUAGES: Languages[] = [
   { name: 'por', image: 'https://flagcdn.com/w320/pt.png' },
   { name: 'ara', image: 'https://flagcdn.com/w320/sa.png' },
+  {
+    name: 'bre',
+    image:
+      'https://media.istockphoto.com/id/495053912/pt/foto/bandeira-da-bretanha-em-fran%C3%A7a.jpg?s=612x612&w=0&k=20&c=2riKEBAqQ34Zs3by2uQh8wwJ96nJpcNLQeF2v8jHaoU=',
+  },
+  { name: 'ces', image: 'https://flagcdn.com/w320/cz.png' },
+  {
+    name: 'cym',
+    image:
+      'https://s5.static.brasilescola.uol.com.br/be/2022/12/bandeira-do-pais-de-gales.jpg',
+  },
+  { name: 'deu', image: 'https://flagcdn.com/w320/de.png' },
+  { name: 'est', image: 'https://flagcdn.com/w320/ee.png' },
+  { name: 'fin', image: 'https://flagcdn.com/w320/fi.png' },
+  { name: 'fra', image: 'https://flagcdn.com/w320/fr.png' },
+  { name: 'hrv', image: 'https://flagcdn.com/w320/hr.png' },
+  { name: 'hun', image: 'https://flagcdn.com/w320/hu.png' },
+  { name: 'jpn', image: 'https://flagcdn.com/w320/jp.png' },
 ];
